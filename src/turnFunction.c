5d895690c5560b2c508391e9fd67dcecc80b5199
invader237 <diego.trivino.fr@gmail.com>
2023-12-31 16:09:04 +0100

    :memo:(turnFunction.c):Added function documentation

--- src/turnFunction.c
+++ src/turnFunction.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int play(int numPlayer, int tab[][3], int C, int L) {
+  /**
+   * play - Function to simulate a turn for a player in a memory matching game.
+   *
+   * This function displays the grid, prompts the player to choose two cards, and checks if the chosen cards match.
+   *
+   * Parameters:
+   *      - numPlayer (int): Player number.
+   *      - tab[][3] (int): 2D array representing the game grid.
+   *      - C (int): Number of columns in the grid.
+   *      - L (int): Number of rows in the grid.
+   *
+   * Local variables:
+   *      - x, y (int): Coordinates for card selection.
+   *      - firstCard, secCard (int): Values of the selected cards.
+   *
+   * Return:
+   *      - int: Always returns 0.
+   */
   int x,y;
   //display grid
   printf("Player %d turn\n", numPlayer);
@@ -21,10 +39,33 @@ int play(int numPlayer, int tab[][3], int C, int L) {
 }
 
 int getValueAt(int x, int y, int tab[][3]) {
+  /**
+   * getValueAt - Function to get the value at a specific position in the grid.
+   *
+   * Parameters:
+   *      - x (int): Column coordinate.
+   *      - y (int): Row coordinate.
+   *      - tab[][3] (int): 2D array representing the game grid.
+   *
+   * Return:
+   *      - int: Value at the specified position in the grid.
+   */
   return tab[x][y];
 }
 
 void getCardCoords(int *x, int *y, int C, int L) {
+  /**
+   * getCardCoords - Function to get the coordinates of a card selected by the player.
+   *
+   * Parameters:
+   *      - x (int*): Pointer to the variable to store the column coordinate.
+   *      - y (int*): Pointer to the variable to store the row coordinate.
+   *      - C (int): Number of columns in the grid.
+   *      - L (int): Number of rows in the grid.
+   *
+   * Return:
+   *      - void
+   */
   printf("\ncolumn: ");
   while (1){
   scanf("%d", x);
@@ -44,6 +85,16 @@ void getCardCoords(int *x, int *y, int C, int L) {
 }
 
 int checkRange(int number, int maxRange) {
+  /**
+   * checkRange - Function to check if a number is within a specified range.
+   *
+   * Parameters:
+   *      - number (int): The number to be checked.
+   *      - maxRange (int): The maximum value allowed in the range.
+   *
+   * Return:
+   *      - int: 1 if the number is within the range, 0 otherwise.
+   */
   if (number<=maxRange && number>=0)return 1; //change the min to number >0 when implementing the CLI
   return 0;
 }
