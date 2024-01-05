c34247d12d0d17918945fde12afbb50c2a8288b7
invader237 <diego.trivino.fr@gmail.com>
2024-01-05 18:05:01 +0100

    :label:(tabGenerationHeader.h): correction of a type error in the header

--- include/tabGenerationHeader.h
+++ include/tabGenerationHeader.h
@@ -4,5 +4,5 @@
 void strucGeneration(int *C, int *L);
 void tabGeneration(int C, int L, int tab[L][C]);
 void tabRandomization(int C , int L, int tab[][C]);
-void randomSwap(int C, int L, int c, int l, int* tab[][C]);
+void randomSwap(int C, int L, int c, int l, int tab[][C]);
 #endif
