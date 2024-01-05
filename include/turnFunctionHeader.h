8eebee66d6a449bf53e35357bb73ca0d8b5d0f1a
invader237 <diego.trivino.fr@gmail.com>
2024-01-05 16:06:00 +0100

    :heavy_plus_sign:(turnFunctionHeader.h): modification of the play function header

--- include/turnFunctionHeader.h
+++ include/turnFunctionHeader.h
@@ -1,8 +1,7 @@
 #ifndef TURN_FUNCTION_H
 #define TURN_FUNCTION_H
 
-int play(int numPlayer, int tab[][3], int C, int L);
-int getValueAt(int x, int y, int tab[][3]);
+int play(int numPlayer, int C, int L, int tab[][C], int layer[][C][2]);
 void getCardCoords(int *x, int *y, int C, int L);
 int checkRange(int number, int maxRange);
 
