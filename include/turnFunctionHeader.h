bf757c140a2a906bd0ddd62004ce5857374e2361
invader237 <diego.trivino.fr@gmail.com>
2024-01-08 11:54:26 +0100

    :art:(mainHeader.h): improvement of header writing

--- include/turnFunctionHeader.h
+++ include/turnFunctionHeader.h
@@ -1,7 +1,7 @@
 #ifndef TURN_FUNCTION_H
 #define TURN_FUNCTION_H
 
-int play(int numPlayer, int C, int L, int tab[][C], int layer[][C][2]);
+int play(int numPlayer, int C, int L, int tab[][C], char layer[][C][2]);
 void getCardCoords(int *x, int *y, int C, int L);
 int checkRange(int number, int maxRange);
 
