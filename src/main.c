01eb5c51ae268bd33184584a716fac4d46dc56da
invader237 <diego.trivino.fr@gmail.com>
2024-01-08 18:35:26 +0100

    :sparkles:(main.c):implementation of bot functionalities

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include "../include/displayFunctionHeader.h"
 #include "../include/turnFunctionHeader.h"
 #include "../include/tabGenerationHeader.h"
+#include "../include/botFunctionHeader.h"
 
 #define CYAN    "\x1b[36m"
 #define GREEN   "\x1b[32m"
@@ -38,6 +39,7 @@ int main(){
   int matchCounter = 0;
   int maxMatch;
   int numberOfPlayer = 0;
+  int numberOfBot;
   int turnScore = 0;
   int playerNumber = 1;
   struct Player players[4] = {{1, 0}, {2, 0}, {3, 0}, {4, 0}};
@@ -62,37 +64,52 @@ int main(){
     printf("Combien de joueur ? (1-4): ");
     scanf("%d", &numberOfPlayer);
   }
-
-  while(checkBot(botAnsw)==-1){
-    printf("Voulez-vous jouer contre un ordinateur (y/n) : ");
-    scanf("%c", &botAnsw);
-  }
-  printf("\033[H\033[J");
-  while(matchCounter!=maxMatch){
-
-    layerDisplay(C, L, layer);
-    turnScore = play(playerNumber, C, L, tab, layer);
-    layerReset(C, L, layer);
-    if(turnScore == 1){
-      printf(GREEN"\nYou have won 1 point, you can play again\n"RESET);
-      players[playerNumber - 1].score += 1;
+  if (numberOfPlayer < 4) {
+        while (checkBot(botAnsw) == -1) {
+            printf("Voulez-vous jouer contre un ordinateur (y/n) : ");
+            scanf(" %c", &botAnsw);
+        }
+        if(checkBot(botAnsw)==1){
+          while (!((numberOfBot <= 4-numberOfPlayer) && numberOfBot>=1)) {
+              printf("Combien de bots ? (1-%d) : ", 4-numberOfPlayer);
+              scanf("%d", &numberOfBot);
+          }
+        }   
     }
-    else if(turnScore == 0){
-      printf(ORANGE"You haven't found a match\n"RESET);
-      matchCounter+=1;
-      playerNumber += 1;
-      if(playerNumber > numberOfPlayer) playerNumber = 1;
-    }
-    else if(turnScore == -1){
-      printf(RED"You have found the joker.\nYou lost a point\n"RESET);
-      playerNumber += 1;
-      if(playerNumber > numberOfPlayer) playerNumber = 1;
 
-      players[playerNumber - 1].score -= 1;
-    }
-    printf("Your score is now : %d\n", players[playerNumber - 1].score);
-    sleep(4);
     printf("\033[H\033[J");
-  }
-  printf("partie terminer");
+    while (matchCounter != maxMatch) {
+        layerDisplay(C, L, layer);
+
+        if (playerNumber <= numberOfPlayer) {
+            // Les joueurs humains jouent
+            turnScore = play(playerNumber, C, L, tab, layer);
+        } else {
+            // Les joueurs bots jouent
+            turnScore = botPlay(playerNumber, C, L, tab, layer);
+        }
+
+        layerReset(C, L, layer);
+        if (turnScore == 1) {
+            printf(GREEN"\nYou have won 1 point, you can play again\n"RESET);
+            matchCounter += 1;
+            players[playerNumber - 1].score += 1;
+        } else if (turnScore == 0) {
+            printf(ORANGE"You haven't found a match\n"RESET);
+            playerNumber += 1;
+            if (playerNumber > numberOfPlayer + numberOfBot) playerNumber = 1;
+        } else if (turnScore == -1) {
+            printf(RED"You have found the joker.\nYou lost a point\n"RESET);
+            playerNumber += 1;
+            if (playerNumber > numberOfPlayer + numberOfBot) playerNumber = 1;
+
+            players[playerNumber - 1].score -= 1;
+        }
+        printf("Your score is now : %d\n", players[playerNumber - 1].score);
+        sleep(4);
+        printf("\033[H\033[J");
+    }
+    printf("partie terminée");
+
+    return 0;
 }
